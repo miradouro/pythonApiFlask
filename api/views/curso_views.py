b1a292fbd08eec69c765986fe63338b58c5cf7f0
Apocalipse <RAFAELMIRADOURO@GMAIL.COM>
2022-10-15 07:35:47 -0300

    15/10/22 - 01

---
+++ api/views/curso_views.py
@@ -0,0 +1,11 @@
+from flask_restful import Resource
+from api import api
+
+
+class CursoList(Resource):
+    def get(self):
+        return 'Hello World!!!'
+
+api.add_resource(CursoList, '/cursos')
+
+
