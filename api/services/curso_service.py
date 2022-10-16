409d4eb518631d8497aa45bfc4bcd009884a79b4
Rafael <you@example.com>
2022-10-16 17:59:20 -0300

    16/10/2022

---
+++ api/services/curso_service.py
@@ -0,0 +1,18 @@
+from ..models import curso_model
+from api import db
+
+
+def cadastrar_curso(curso):
+    curso_bd = curso_model.Curso(
+        nome=curso.nome,
+        descricao=curso.descricao,
+        data_publicacao=curso.data_publicacao
+    )
+    db.session.add(curso_bd)
+    db.session.commit()
+    return curso_bd
+
+
+def listar_cursos():
+    cursos = curso_model.Curso.query.all()
+    return cursos
